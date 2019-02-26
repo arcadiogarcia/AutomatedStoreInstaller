0a7269a6388d1c72897bc3f9e24a569168661776
Arcadio Garcia Salvadores <9416356+arcadiogarcia@users.noreply.github.com>
2019-02-26 11:16:45 -0800

    Create AutomatedStoreInstaller.ps1

---
+++ AutomatedStoreInstaller.ps1
@@ -0,0 +1,17 @@
+ Param(
+[Parameter(Mandatory=$True)]
+[string]$jsonConfigurationFile
+)
+
+$jsondata = Get-Content -Raw -Path $jsonConfigurationFile | ConvertFrom-Json
+foreach ($app in $jsondata.apps)
+{
+$url = "ms-windows-store://P
+DP/?productid="+$app
+Start-Process -FilePath $url
+$wshell = New-Object -ComObject wscript.shell;
+$wshell.AppActivate('Microsoft Store')
+Sleep $jsondata.delay
+$wshell.SendKeys(' ')
+Sleep $jsondata.delay
+}   
