322773b9e8ee88a2f9c2d168802cd7298dc89da8
Arcadio Garcia Salvadores <9416356+arcadiogarcia@users.noreply.github.com>
2019-02-26 11:31:07 -0800

    I messed up because of course I did

--- AutomatedStoreInstaller.ps1
+++ AutomatedStoreInstaller.ps1
@@ -6,12 +6,11 @@
 $jsondata = Get-Content -Raw -Path $jsonConfigurationFile | ConvertFrom-Json
 foreach ($app in $jsondata.apps)
 {
-$url = "ms-windows-store://P
-DP/?productid="+$app
-Start-Process -FilePath $url
-$wshell = New-Object -ComObject wscript.shell;
-$wshell.AppActivate('Microsoft Store')
-Sleep $jsondata.delay
-$wshell.SendKeys(' ')
-Sleep $jsondata.delay
+    $url = "ms-windows-store://PDP/?productid="+$app
+    Start-Process -FilePath $url
+    $wshell = New-Object -ComObject wscript.shell;
+    $wshell.AppActivate('Microsoft Store')
+    Sleep $jsondata.delay
+    $wshell.SendKeys(' ')
+    Sleep $jsondata.delay
 }   
